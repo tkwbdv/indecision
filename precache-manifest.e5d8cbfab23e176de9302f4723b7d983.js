7334a98e97e295cf62646654cd8c1b85ed4a4a0f
ThoKur <Mick.18@gmx.de>
2019-06-30 13:50:31 +0200

    Updates

---
+++ precache-manifest.e5d8cbfab23e176de9302f4723b7d983.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fb0e6250136a63e6843d48c6a66d9a0",
+    "url": "/indecision/index.html"
+  },
+  {
+    "revision": "452e91e0289e1fd7f535",
+    "url": "/indecision/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "ae450da835a36b3ee579",
+    "url": "/indecision/static/css/main.2ad70a42.chunk.css"
+  },
+  {
+    "revision": "452e91e0289e1fd7f535",
+    "url": "/indecision/static/js/2.36cf1d7f.chunk.js"
+  },
+  {
+    "revision": "ae450da835a36b3ee579",
+    "url": "/indecision/static/js/main.27795700.chunk.js"
+  },
+  {
+    "revision": "666c8f8221d665c9b1cf",
+    "url": "/indecision/static/js/runtime~main.401c7c33.js"
+  }
+]);
